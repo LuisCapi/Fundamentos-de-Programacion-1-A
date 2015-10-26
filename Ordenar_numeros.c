48b25c7874c219a32b882bc83f7b4f23b936888f
LuisCapi <luis97.capi@gmail.com>
2015-10-26 17:50:25 -0600

    Programa que ordena numeros de mayor a menor

---
+++ Ordenar_numeros.c
@@ -0,0 +1,65 @@
+#include <stdio.h> 
+int n1;
+int n2;
+int n3;
+int main() 
+{ 
+   	printf("Programa que ordena los numeros de mayor a menor\n");
+	printf("Introduce el primer numero: "); 
+    scanf("%d",&n1); 
+    printf("Introduce el segundo numero: "); 
+    scanf("%d",&n2); 
+    printf("Introduce el tercer numero: "); 
+    scanf("%d",&n3); 
+     
+    if(n1>=n2 && n1>=n3) 
+    { 
+    if(n2>=n3) 
+    { 
+    printf("Numero mayor: %d\n",n1); 
+    printf("Numero intermedio: %d\n",n2); 
+    printf("Numero menor: %d\n",n3); 
+    } 
+               
+            else  if(n3>=n2) 
+              { 
+                       printf("Numero mayor: %d\n",n1); 
+                       printf("Numero intermedio: %d\n",n3); 
+                       printf("Numero menor: %d\n",n2); 
+              } 
+    } 
+     
+    if(n2>=n1 && n2>=n3) 
+    { 
+	if(n1>=n3) 
+    { 
+    printf("Numero mayor: %d\n",n2); 
+    printf("Numero intermedio: %d\n",n1); 
+	printf("Numero menor: %d\n",n3); 
+    } 
+               
+            else if(n3>=n1) 
+              { 
+                       printf("Numero mayor: %d\n",n2); 
+                       printf("Numero intermedio: %d\n",n3); 
+                       printf("Numero menor: %d\n",n1); 
+              } 
+    } 
+     
+    if(n3>=n1 && n3>=n2) 
+    { 
+    if(n1>=n2) 
+    { 
+    printf("Numero mayor: %d\n",n3); 
+    printf("Numero intermedio: %d\n",n1); 
+    printf("Numero menor: %d\n",n2); 
+    } 
+               
+            else if(n2>=n1) 
+              { 
+                       printf("Numero mayor: %d\n",n3); 
+                       printf("Numero intermedio: %d\n",n2); 
+                       printf("Numero menor: %d\n",n1); 
+              } 
+    } 
+} 
