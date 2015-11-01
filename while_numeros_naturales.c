5ba9d68463509001258f955c3e61f5c1d2cb5b55
LuisCapi <luis97.capi@gmail.com>
2015-10-31 23:46:33 -0600

    Promagrama que suma digitos con el uso de while

--- while_numeros_naturales.c
+++ while_numeros_naturales.c
@@ -4,17 +4,20 @@
 
 int final;
 int i;
+int suma;
 int main()
 {
 printf("Ingresa el numero final: ");
 scanf("%d",&final);
 i=1;
+suma=0;
 while(i<=final)
 {
 	printf("%d\n",i);
+	suma=suma+i;
 	i=i+1;
 }
-{
+	printf("Suma= %d\n",suma);
+	
 	printf("La suma es: %d",(final*(final+1)/2)); /*Use la formula n(n+1)/2 para la suma*/
- }
 }
