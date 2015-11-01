8c546d44798b44c6cf093b8d6883bbf1528dcc6c
LuisCapi <luis97.capi@gmail.com>
2015-10-31 21:21:05 -0600

    Programa con el uso de do while

---
+++ do_while.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int n1;
+int n2;
+int i;
+int suma;
+int main()
+{	printf("Programa que suma numeros para realizar una multiplicacion\n");
+	printf("ingresa el primer digito de la multiplicacion: ");
+	scanf("%d",&n1);
+	printf("ingresa el segundo digito de la multiplicacion: ");
+	scanf("%d",&n2);
+i=1;
+suma=0;
+do
+{	printf(" %d\n",n2);	
+	suma=suma+n2;
+	i++;
+}
+while(i<=n1);
+printf("\nLa respuesta es: %d",suma);
+}
+
