b75ef1900c38ca7606128d4a507f35f25349b258
LuisCapi <luis97.capi@gmail.com>
2015-11-05 16:28:27 -0600

    Programa que imprime los 10 primeros numeros con el ciclo for

---
+++ ciclo_for.c
@@ -0,0 +1,12 @@
+/*ciclo FOR sirve para contar*/
+/*Desde donde,	asta donde se va a llegar, incremento*/
+#include<stdio.h>
+int i;
+int main()
+{
+printf("Programa que imprime los primeros 10 numeros:\n");
+for(i=1;i<=10;i++)
+{
+	printf("%d\n",i);
+}
+}
