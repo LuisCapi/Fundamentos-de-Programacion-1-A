f5d93b228ee9ea57e4e9ea06a3f7529a769c5561
LuisCapi <luis97.capi@gmail.com>
2015-11-18 23:52:05 -0600

    Programa que saca el promedio y moda de 10 calificaciones

--- promedio_y_moda.c
+++ promedio_y_moda.c
@@ -1,3 +1,4 @@
+//se volvio a subir el programa, ya que tenia algunos detalles incorrectos.
 #include<stdio.h>
 int calificaciones[10],suma;
 float promedio;
@@ -18,23 +19,22 @@ int main()
 	suma=suma+calificaciones[i];	
 	}
 	promedio=(float)suma/10;
-	printf("EL promedio es:%f",promedio);
+	printf("\nEL promedio es:%f\n",promedio);
 
 	for(i=0;i<10;i++)
 	{
 		moda[calificaciones[i]-1]++;
 	}
-mayor=moda[i];
+mayor=moda[0];//modificacion
 pos=0;
-for(i=0;i<10;i++)
+for(i=1;i<10;i++)//modificacion
 {
-    if (moda[i]> mayor){
+    if (moda[i]> mayor)
+	{
     mayor=moda[i];
     pos=i+1;
-    
     }
 }
 printf("\n");
 printf("Moda:%d\n",pos);
-
 }
